e9e1fa7403c955be349850ace883886a44301342
512pretzels-Guy <512pretzels@gmail.com>
2026-04-27 16:05:58 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'lotr-vault-v1';
+const ASSETS = [
+  './LOTR1.html',
+  './manifest.json'
+];
+
+// Install Service Worker
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Intercept requests to serve from Cache
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
